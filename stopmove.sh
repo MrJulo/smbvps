484579a8b8dd274307fccb7ec7dbb663bd6d31ef
MrJulo <sanmaoban@hotmail.com>
2024-01-13 13:43:42 +0800

    Update stopmove.sh

--- stopmove.sh
+++ stopmove.sh
@@ -3,9 +3,9 @@
 # 查找与 move.sh 相关的进程组并终止
 pids=$(ps aux | grep 'move.sh' | grep -v 'grep' | awk '{print $2}')
 
-if [ -n "$pids" ]; then
-    echo "Terminating processes with PIDs: $pids"
-    pkill -TERM -g "$pids"
+if [ -n "$pid" ]; then
+    echo "Terminating process with PID: $pid"
+    kill "$pid"
 else
-    echo "No matching processes found."
+    echo "No matching process found."
 fi
