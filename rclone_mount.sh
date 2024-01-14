fd36cff2d9df1a582e45057345110623053f71b3
MrJulo <sanmaoban@hotmail.com>
2024-01-14 19:17:40 +0800

    Update rclone_mount.sh

--- rclone_mount.sh
+++ rclone_mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fixed_path="/var/lib/transmission-daemon/downloads/"
-fixed_options="--allow-other --vfs-cache-mode writes --vfs-cache-max-size 5G --buffer-size 32M --umask 000"
+fixed_options="--allow-other --vfs-cache-mode writes --vfs-cache-max-size 1G --buffer-size 32M --umask 000"
 services=("disk-m" "disk1" "disk2" "disk3" "disk4" "disk5")
 for service in "${services[@]}"; do
     if [ -d "$fixed_path$service" ]; then
