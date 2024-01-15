f0869d23d5620fcb3ed9dd5ba8de9647bc7ecda0
MrJulo <sanmaoban@hotmail.com>
2024-01-16 03:30:01 +0800

    Update rclone_move.sh

--- rclone_move.sh
+++ rclone_move.sh
@@ -75,7 +75,7 @@ while true; do
         selected_item=$(basename "${files[$selected_index]}")
 
         # 使用rclone move命令移动文件夹，保持文件夹结构
-        nohup bash -c "echo -e '\e[1;36m项目 $selected_item\e[0m' && rclone move \"$source_path/$selected_item\" \"$destination_path/$selected_item\" --progress --stats-one-line --stats 5s && echo -e '\e[1;36m项目成功移动到 $destination_path/$selected_item!\e[0m'" > "out$counter" 2>&1 &
+        nohup bash -c "echo -e '\e[1;36m项目 $selected_item\e[0m' && rclone move \"$source_path/$selected_item\" \"$destination_path/$selected_item\" --progress --stats-one-line --stats 5s && echo -e '\e[1;36m项目成功移动到 $destination_path/$selected_item!\e[0m'" > "outp$counter" 2>&1 &
         echo -e "\e[1;36m正在移动文件 $selected_item，请查看 outp$counter 文件以获取详细信息。\e[0m"
         ((counter++))
     else
